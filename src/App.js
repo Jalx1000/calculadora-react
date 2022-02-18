2e7c661187ae3eabd9d5d66c839c578b41281089
JAL <jalejandromogro1@gmail.com>
2022-02-18 17:01:57 -0400

    Funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,31 +1,82 @@
 import './App.css';
+import React, {useState} from 'react'
+
+
 
 function App() {
+
+  const [number1,setNumber1] = useState("");
+  const [number2,setNumber2] = useState("");
+  const [currentOperation,setCurrentOperation] = useState("");
+  const [result,setResult] = useState(0);
+
+  function allClear(){
+    setNumber1("");
+    setNumber2("");
+    setCurrentOperation("");
+    setResult("");
+  }
+
+  function deleteNumber(){
+    if (currentOperation==""){
+      setNumber1(number1.toString().slice(0,-1))
+    }else {
+      setNumber2(number2.toString().slice(0,-1))
+    }
+  }
+  function clickNumber(val){
+    if(currentOperation === ""){
+      setNumber1(number1 + val);
+    }else{
+      setNumber2(number2 + val);
+    }
+  }
+
+  function clickOperation(val){
+    setCurrentOperation(val);
+  }
+  
+  function getResult (){
+    switch(currentOperation){
+      case "+": 
+        setResult(Number(number1)+Number(number2));
+      break;
+        case "-":
+        setResult(Number(number1)-Number(number2));
+      break;
+      case "/": 
+        setResult(Number(number1)/Number(number2));
+      break;
+      case "*": 
+        setResult(Number(number1)*Number(number2));
+      break;
+    }
+  }
   return (
     <div className="App">
       <div className="calculator-grid">
         <div className="output">
-          <div className="previous-operand">123</div>
-          <div className="current-operand">789</div>
+          <div className="previous-operand">{currentOperation ? number1 + currentOperation:""}</div>
+          <div className="current-operand">{result ? result: (!currentOperation ? number1:number2)}</div>
         </div>
-        <button className="doble">AC</button>
-        <button>DEL</button>
-        <button>/</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button className="doble">=</button>
+        <button onClick={allClear} className="doble">AC</button>
+        <button onClick={deleteNumber}>DEL</button>
+        <button onClick={()=> {clickOperation("/")}}>/</button>
+        <button onClick={() => {clickNumber(7)}}>7</button>
+        <button onClick={() => {clickNumber(8)}}>8</button>
+        <button onClick={() => {clickNumber(9)}}>9</button>
+        <button onClick={()=> {clickOperation("*")}}>*</button>
+        <button onClick={() => {clickNumber(4)}}>4</button>
+        <button onClick={() => {clickNumber(5)}}>5</button>
+        <button onClick={() => {clickNumber(6)}}>6</button>
+        <button onClick={()=> {clickOperation("+")}}>+</button>
+        <button onClick={() => {clickNumber(1)}}>1</button>
+        <button onClick={() => {clickNumber(2)}}>2</button>
+        <button onClick={() => {clickNumber(3)}}>3</button>
+        <button onClick={()=> {clickOperation("-")}}>-</button>
+        <button onClick={()=> {clickOperation(".")}}>.</button>
+        <button onClick={()=> {clickNumber(0)}}>0</button>
+        <button onClick={getResult} className="doble">=</button>
       </div>
     </div>
   );
